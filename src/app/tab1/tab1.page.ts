dd283e331b0a350b7b1542f96f5814b6734d0749
jakubforman <me@jakubforman.eu>
2024-04-25 15:38:37 +0200

    Enhance Tab1Page with modal, country search, and currency filter
    The Tab1Page has been heavily extended with new functionalities and model references for a better user experience. These include the addition of an ion modal, a search function to filter through countries, and a feature to filter by currencies. The MapCurrency model is also now imported alongside MapCountry and the control flow for initialising countries is updated. These changes aim to make the user's navigation and the data presentation on the Tab1Page more efficient and enjoyable.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
-import {CountriesService, MapCountry} from "../services/countries/countries.service";
-import {Observable} from "rxjs";
+import {Component, ViewChild} from '@angular/core';
+import {CountriesService, MapCountry, MapCurrency} from "../services/countries/countries.service";
+import {Observable, of, tap} from "rxjs";
 import {Country} from "../models/country.model";
+import {IonModal} from "@ionic/angular";
 
 @Component({
   selector: 'app-tab1',
@@ -9,16 +10,59 @@ import {Country} from "../models/country.model";
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonModal) modal!: IonModal;
 
   countries$: Observable<MapCountry[]>;
+
+  private countriesOrigin: MapCountry[] = [];
+
+  currencies: MapCurrency[] = [];
 
   constructor(
     private countriesService: CountriesService
   ) {
-    this.countries$ = this.countriesService.index$();
+    this.countries$ = this.countriesService.index$()
+      .pipe(tap(countries => {
+        this.countriesOrigin = countries;
+        this.currencies = this.filterCurrencies(countries);
+      }));
   }
 
   transferCountry(country: MapCountry) {
     this.countriesService.country = country;
+  }
+
+  searchCountry(event: CustomEvent) {
+    const value = event.detail.value.toLowerCase();
+
+    const filteredCountries = this.countriesOrigin.filter(country => {
+      return country.name.official.toLowerCase().includes(value);
+    })
+
+    this.countries$ = of(filteredCountries);
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(null, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent;
+    if (ev.detail.role === 'confirm') {
+      console.log("Okno zavřeno");
+    }
+  }
+
+  filterCurrencies(countries: MapCountry[]): MapCurrency[] {
+    // @ts-ignore
+    return countries.flatMap(c => c.currencies)
+      .reduce((acc: MapCurrency[], curr: MapCurrency) => {
+        const duplicate = acc.find(item => item.key === curr.key);
+        return duplicate ? acc : [...acc, curr];
+      }, []);
   }
 }
